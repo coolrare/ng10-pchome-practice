8aa8709fd81dbf544c4b84abb9b82e3a58618f5f
黃升煌 Mike <mike@miniasp.com>
2020-08-06 14:15:29 +0800

    事件繫結 & 雙向繫結 #6 #7

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  keyword = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  search(event: MouseEvent): void {
+    console.log(event);
+
+    if (event.altKey) {
+      console.log('test');
+    }
   }
-
 }
