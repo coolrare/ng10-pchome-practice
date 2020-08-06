a6fe7937024256f57ed213789849f749853ec2e7
黃升煌 Mike <mike@miniasp.com>
2020-08-06 13:51:31 +0800

    內嵌繫結 & 屬性繫結 #4, #5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng10-pchome-practice';
+  title = 'conduit!';
+  subtitle = 'A place to share your <u>knowledge</u>.';
 }
